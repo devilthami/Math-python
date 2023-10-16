b648c9f57d0add48c563badff69f08b490de5f72
P@rashmgr <96672535+devilthami@users.noreply.github.com>
2023-10-16 21:43:44 +0545

    Add files via upload

---
+++ Python_Math.py
@@ -0,0 +1,19 @@
+x = min(5, 10, 20)
+y = max(5, 10, 20)
+
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4, 3)
+
+print(x)
+
+import math
+x = math.sqrt(64)
+print(x)
+
+x = math.pi
+print(x)
